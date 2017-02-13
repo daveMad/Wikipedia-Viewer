7e8b623dafde17b6fcfc4d8707d864a4345fa0a2
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-13 05:15:04 +0200

    add rxjs extensions

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,3 +2,9 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
+@Injectable()
+export class AppService {
+    constructor(private http : Http){
+        
+    }
+}
